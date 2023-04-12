9d5060a343e56cad3e5d3c9ddb448b668123f2a8
Julien Maitrehenry <julien@kumojin.com>
2023-04-12 16:41:27 -0400

    Add new matrix simulator and move the CGO dependency for linux only

--- matrix.go
+++ matrix.go
@@ -1,51 +1,14 @@
 package rgbmatrix
 
-/*
-#cgo CFLAGS: -std=c99 -I${SRCDIR}/lib/rpi-rgb-led-matrix/include -DSHOW_REFRESH_RATE
-#cgo LDFLAGS: -lrgbmatrix -L${SRCDIR}/lib/rpi-rgb-led-matrix/lib -lstdc++ -lm
-#include "lib/rpi-rgb-led-matrix/include/led-matrix-c.h"
-
-void led_matrix_swap(struct RGBLedMatrix *matrix, struct LedCanvas *offscreen_canvas,
-                     int width, int height, const uint32_t pixels[]) {
-
-
-  int i, x, y;
-  uint32_t color;
-  for (x = 0; x < width; ++x) {
-    for (y = 0; y < height; ++y) {
-      i = x + (y * width);
-      color = pixels[i];
-
-      led_canvas_set_pixel(offscreen_canvas, x, y,
-        (color >> 16) & 255, (color >> 8) & 255, color & 255);
-    }
-  }
-
-  offscreen_canvas = led_matrix_swap_on_vsync(matrix, offscreen_canvas);
-}
-
-void set_show_refresh_rate(struct RGBLedMatrixOptions *o, int show_refresh_rate) {
-  o->show_refresh_rate = show_refresh_rate != 0 ? 1 : 0;
-}
-
-void set_disable_hardware_pulsing(struct RGBLedMatrixOptions *o, int disable_hardware_pulsing) {
-  o->disable_hardware_pulsing = disable_hardware_pulsing != 0 ? 1 : 0;
-}
-
-void set_inverse_colors(struct RGBLedMatrixOptions *o, int inverse_colors) {
-  o->inverse_colors = inverse_colors != 0 ? 1 : 0;
-}
-*/
 import "C"
 import (
-	"fmt"
+	"github.com/jmaitrehenry/go-rpi-rgb-led-matrix/emulator"
+	"github.com/jmaitrehenry/go-rpi-rgb-led-matrix/julien"
+	"github.com/jmaitrehenry/go-rpi-rgb-led-matrix/terminal"
 	"image/color"
 	"os"
+	"runtime"
 	"strings"
-	"unsafe"
-
-	"github.com/jmaitrehenry/go-rpi-rgb-led-matrix/emulator"
-	"github.com/jmaitrehenry/go-rpi-rgb-led-matrix/terminal"
 )
 
 // DefaultConfig default WS281x configuration
@@ -109,40 +72,6 @@ func (c *HardwareConfig) geometry() (width, height int) {
 	return c.Cols * c.ChainLength, c.Rows * c.Parallel
 }
 
-func (c *HardwareConfig) toC() *C.struct_RGBLedMatrixOptions {
-	o := &C.struct_RGBLedMatrixOptions{}
-	o.rows = C.int(c.Rows)
-	o.cols = C.int(c.Cols)
-	o.chain_length = C.int(c.ChainLength)
-	o.parallel = C.int(c.Parallel)
-	o.pwm_bits = C.int(c.PWMBits)
-	o.pwm_lsb_nanoseconds = C.int(c.PWMLSBNanoseconds)
-	o.brightness = C.int(c.Brightness)
-	o.scan_mode = C.int(c.ScanMode)
-	o.hardware_mapping = C.CString(c.HardwareMapping)
-	o.pixel_mapper_config = C.CString(c.PixelMapperConfig)
-
-	if c.ShowRefreshRate == true {
-		C.set_show_refresh_rate(o, C.int(1))
-	} else {
-		C.set_show_refresh_rate(o, C.int(0))
-	}
-
-	if c.DisableHardwarePulsing == true {
-		C.set_disable_hardware_pulsing(o, C.int(1))
-	} else {
-		C.set_disable_hardware_pulsing(o, C.int(0))
-	}
-
-	if c.InverseColors == true {
-		C.set_inverse_colors(o, C.int(1))
-	} else {
-		C.set_inverse_colors(o, C.int(0))
-	}
-
-	return o
-}
-
 type ScanMode int8
 
 const (
@@ -158,49 +87,14 @@ type RGBLedMatrix struct {
 	width  int
 	matrix *C.struct_RGBLedMatrix
 	buffer *C.struct_LedCanvas
-	leds   []C.uint32_t
+	leds   []uint32_t
 }
 
 const MatrixEmulatorENV = "MATRIX_EMULATOR"
 const TerminalMatrixEmulatorENV = "MATRIX_TERMINAL_EMULATOR"
 
-// NewRGBLedMatrix returns a new matrix using the given size and config
-func NewRGBLedMatrix(config *HardwareConfig) (c Matrix, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			var ok bool
-			err, ok = r.(error)
-			if !ok {
-				err = fmt.Errorf("error creating matrix: %v", r)
-			}
-		}
-	}()
-
-	if isMatrixEmulator() {
-		return buildMatrixEmulator(config), nil
-	}
-	if isTerminalMatrixEmulator() {
-		return buildTerminalMatrixEmulator(config), nil
-	}
-
-	m := C.led_matrix_create_from_options(config.toC(), nil, nil)
-	b := C.led_matrix_create_offscreen_canvas(m)
-
-	var w, h C.int
-	C.led_canvas_get_size(b, &w, &h)
-
-	c = &RGBLedMatrix{
-		Config: config,
-		width:  int(w), height: int(h),
-		matrix: m,
-		buffer: b,
-		leds:   make([]C.uint32_t, int(w)*int(h)),
-	}
-	if m == nil {
-		return nil, fmt.Errorf("unable to allocate memory")
-	}
-
-	return c, nil
+func isJulienEmulator() bool {
+	return runtime.GOOS == "darwin"
 }
 
 func isMatrixEmulator() bool {
@@ -215,8 +109,13 @@ func isTerminalMatrixEmulator() bool {
 	if os.Getenv(TerminalMatrixEmulatorENV) == "1" {
 		return true
 	}
-
 	return false
+}
+
+func buildJulienMatrix(config *HardwareConfig) Matrix {
+	w, h := config.geometry()
+	matrix := julien.GenerateEmpty(h, w)
+	return &matrix
 }
 
 func buildMatrixEmulator(config *HardwareConfig) Matrix {
@@ -233,7 +132,7 @@ func buildTerminalMatrixEmulator(config *HardwareConfig) Matrix {
 	return terminal.NewTerminal(w, h, true)
 }
 
-// Initialize initialize library, must be called once before other functions are
+// Initialize library, must be called once before other functions are
 // called.
 func (c *RGBLedMatrix) Initialize() error {
 	return nil
@@ -242,57 +141,6 @@ func (c *RGBLedMatrix) Initialize() error {
 // Geometry returns the width and the height of the matrix
 func (c *RGBLedMatrix) Geometry() (width, height int) {
 	return c.width, c.height
-}
-
-// Apply set all the pixels to the values contained in leds
-func (c *RGBLedMatrix) Apply(leds []color.Color) error {
-	for position, l := range leds {
-		c.Set(position, l)
-	}
-
-	return c.Render()
-}
-
-// Render update the display with the data from the LED buffer
-func (c *RGBLedMatrix) Render() error {
-	w, h := c.Geometry()
-
-	C.led_matrix_swap(
-		c.matrix,
-		c.buffer,
-		C.int(w), C.int(h),
-		(*C.uint32_t)(unsafe.Pointer(&c.leds[0])),
-	)
-
-	c.leds = make([]C.uint32_t, w*h)
-	return nil
-}
-
-// At return an Color which allows access to the LED display data as
-// if it were a sequence of 24-bit RGB values.
-func (c *RGBLedMatrix) At(position int) color.Color {
-	return uint32ToColor(c.leds[position])
-}
-
-// Set set LED at position x,y to the provided 24-bit color value.
-func (c *RGBLedMatrix) Set(position int, color color.Color) {
-	c.leds[position] = C.uint32_t(colorToUint32(color))
-}
-
-// Close finalizes the ws281x interface
-func (c *RGBLedMatrix) Close() error {
-	C.led_matrix_delete(c.matrix)
-	return nil
-}
-
-// GetBrightness returns the current brightness setting of the matrix
-func (c *RGBLedMatrix) GetBrightness() int {
-	return int(C.led_matrix_get_brightness(c.matrix))
-}
-
-// SetBrightness sets a new brightness setting to the matrix
-func (c *RGBLedMatrix) SetBrightness(brightness int) {
-	C.led_matrix_set_brightness(c.matrix, C.uchar(brightness))
 }
 
 func colorToUint32(c color.Color) uint32 {
@@ -305,7 +153,7 @@ func colorToUint32(c color.Color) uint32 {
 	return (red>>8)<<16 | (green>>8)<<8 | blue>>8
 }
 
-func uint32ToColor(u C.uint32_t) color.Color {
+func uint32ToColor(u uint32_t) color.Color {
 	return color.RGBA{
 		uint8(u>>16) & 255,
 		uint8(u>>8) & 255,
